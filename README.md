8e575c763a938ee3c4cafeb29da6070392c06e0f
cmungall <cjm@berkeleybop.org>
2021-04-19 19:37:38 -0700

    initial-commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# mixs-source
+
+DEMO!!!
+
+This repo contains the MIxS schema for environmental packages and checklists
+
+## Deploying Documentation
+
+
+```bash
+cp docs/*.md ../gensc.github.io/mixs6/
+```
---
+++ gsctools/mixs_converter.py
@@ -0,0 +1,426 @@
+
+"""
+converts mixs6 spreadsheet
+
+spreadsheet:
+https://docs.google.com/spreadsheets/d/1QDeeUcDqXes69Y2RjU2aWgOpCVWo5OVsBX9MKmMqi_o/edit#gid=567040283
+
+Note: should also work for mixs5 xlsx files too, once exported to tsv
+"""
+import click
+import yaml
+from dataclasses import dataclass, field
+from typing import Optional, Set, List, Union, Dict, Any, Tuple
+from linkml.utils.formatutils import underscore
+import pandas as pd
+import logging
+from gsctools.packageutils import new_schema
+import os
+import re
+
+CORE_PACKAGE_NAME = 'core'
+
+# technology-specific checklists from the previous MIGS and MIMS standards
+CHECKLISTS = {'migs_eu':
+                  {'name': 'MIGS eukaryote',
+                   'fullname': 'Minimal Information about a Genome Sequence: eukaryote',
+                    'abbrev': 'MIGS.eu'},
+              'migs_ba':
+                  {'name': 'MIGS bacteria',
+                   'fullname': 'Minimal Information about a Genome Sequence: cultured bacteria/archaea',
+                    'abbrev': 'MIGS.ba'
+                   },
+              'migs_pl':
+                  {'name': 'MIGS plant',
+                   'fullname': 'Minimal Information about a Genome Sequence: plant',
+                   'abbrev': 'MIGS.pl'},
+              'migs_vi':
+                  {'name': 'MIGS virus',
+                   'fullname': 'Minimal Information about a Genome Sequence: cultured bacteria/archaea',
+                   'abbrev': 'MIGS.ba'
+                   },
+              'migs_org':
+                  {'name': 'MIGS org',
+                   'fullname': 'Minimal Information about a Genome Sequence: org',
+                   'abbrev': 'MIGS.org'},
+              'me':
+                  {'name': 'ME',
+                   'fullname': 'Metagenome or Environmental',
+                   'abbrev': 'ME'},
+              'mimarks_s':
+                  {'name': 'MIMARKS specimen',
+                   'fullname': 'Minimal Information about a Marker Specimen: specimen',
+                   'abbrev': 'MIMARKS.specimen'},
+              'mimarks_c':  ## TODO check this
+                  {'name': 'MIMARKS survey',
+                   'fullname': 'Minimal Information about a Marker Specimen: survey',
+                   'abbrev': 'MIMARKS.survey'},
+              'misag': {
+                  'name': 'MISAG',
+                  'fullname': 'Minimum Information About a Single Amplified Genome',
+                  'abbrev': 'MISAG'
+              },
+              'mimag': {
+                  'name': 'MIMAG',
+                  'fullname': 'Minimum Information About a Metagenome-Assembled Genome',
+                  'abbrev': 'MIMAG'
+              },
+              'miuvig': {
+                  'name': 'MIUVIG',
+                  'fullname': 'Minimum Information About an Uncultivated Virus Genome',
+                  'abbrev': 'MIUVIG'}
+              }
+
+
+datatype_schema = {
+    'classes':
+        {'quantity value':
+             {'description': 'used to record a measurement',
+              'attributes': {
+                  'has unit': {
+                      'description': 'Example "m"'
+                  },
+                  'has numeric value': {
+                      'range': 'double'
+                  },
+                  'has raw value': {
+                      'string_serialization': '{has numeric value} {has unit}'
+                  }
+              }
+            }
+        }
+}
+
+def safe(s: str) -> str:
+    """
+    render a string safe for use as a python variable
+
+    :param s:
+    :return:
+    """
+    if s[0].isdigit():
+        s = f"x_{s}"
+    if '/' in s:
+        s = s.replace("/", "_")
+    return s
+
+def parse_value_syntax(s: str) -> Tuple[str,str]:
+    pattern = s
+    range = 'string'
+    if s == '{float} {unit}':
+        return None, 'quantity value'
+    elif s == '{float}':
+        return None, 'double'
+    return pattern, range
+
+@dataclass
+class MIxS6Converter:
+    """
+    converts TSV from MIxS spreadsheet into LinkML yaml
+    """
+
+    core_filename: Optional[str] = None
+    packages_filename: Optional[str] = None
+    output_directory: str = './src/schema'
+
+    def convert_and_save(self, fn: str = None) -> str:
+        obj = self.convert()
+        if fn is None:
+            fn = os.path.join(self.output_directory, "mixs.yaml")
+        with open(fn, 'w') as stream:
+            yaml.safe_dump(obj, stream, sort_keys=False)
+        return fn
+
+    def save_schema(self, schema, output) -> None:
+        f = f'{self.output_directory}/{output}'
+        with open(f, 'w') as ostream:
+            yaml.safe_dump(schema, ostream, sort_keys=False)
+
+    def create_slot(self, row, enums: dict = {}) -> (str, Dict):
+        """
+        turn a row from EITHER core tab OR packages tab into a slot definition
+
+        :param row:
+        :return: tuple of id and definition dictionary
+        """
+        s_id = row['Structured comment name']
+        if s_id is None or s_id == '-':
+            logging.error(f"Bad row: {row}")
+            return None, None
+        if ';' in s_id:
+            logging.error(f'Bad ID / SCN: {s_id} in {row}')
+            return None, None
+        if 'Item' in row:
+            s_name = row['Item']
+        elif 'Package item' in row:
+            s_name = row['Package item']
+        else:
+            s_name = row['Item (rdfs:label)']
+        if s_name == '':
+            logging.error(f'No name: {s_id}')
+            return None, None
+        if ';' in s_name:
+            logging.error(f'Bad name: {s_name} in {row}')
+            return None, None
+        comments = []
+        for k in ('Expected value', 'Preferred unit', 'Occurrence', 'Position'):
+            if k in row:
+                comments.append(f'{k}: {row[k]}')
+
+        # the column header is not consistent between sheets here
+        slot_uri = None
+        if 'Unique MIXS ID' in row and row['Unique MIXS ID'] is not None:
+            slot_uri = row['Unique MIXS ID']
+        elif 'unique MIXS ID' in row and row['unique MIXS ID'] is not None:
+            slot_uri = row['unique MIXS ID']
+        elif 'MIXS ID' in row and row['MIXS ID'] is not None:
+            slot_uri = row['MIXS ID']
+        else:
+            None
+            #logging.error(f'No ID: {slot_uri}')
+
+        section = row['Section'] if 'Section' in row else 'environment'
+        if section == '':
+            logging.warning(f'No section: {s_id}')
+            section = 'core'
+        is_a = f'{section} field'
+        pattern, range = parse_value_syntax(row['Value syntax'])
+        slot = {
+            'is_a': is_a,
+            'aliases': [s_name],
+            'description': row['Definition'],
+            'range': range,
+            'examples': [
+                {'value': row['Example']}
+            ],
+            'comments': comments
+        }
+        if pattern is not None:
+            slot['pattern'] = pattern
+        LINK = 'Link to GH issue'
+        if LINK in row:
+            url = row[LINK]
+            if url is not None and url.startswith("http"):
+                slot['see_also'] = url
+
+        s_id = safe(s_id)
+        if pattern is not None and '|' in pattern:
+            vals = pattern.replace('[', '').replace(']','').split('|')
+            vals = [v.strip() for v in vals]
+            # remove entries like '[{PMID}|{DOI}|...]'
+            vals = [v for v in vals if not v.startswith('{')]
+            if len(vals) > 2:
+                enum_name = f'{s_id}_enum'
+                slot['range'] = enum_name
+                enums[enum_name] = {
+                    'permissible_values': {v: {} for v in vals}
+                }
+        if slot_uri is not None and slot_uri != '':
+            slot['slot_uri'] = slot_uri
+        if 'Expected value' in row:
+            range = row['Expected value']
+
+        if 'Section' in row:
+            row['in_subset'] = [row['Section']]
+
+        return (s_id, slot)
+
+    def convert(self) -> Dict[str, Any]:
+        """
+        Converts set of inputs to a schema
+
+        :return: link schema as a python Dictionary
+        """
+        core_df = pd.read_csv(self.core_filename, sep="\t").fillna("")
+        pkg_df  = pd.read_csv(self.packages_filename, sep="\t").fillna("")
+        slots = {
+            'core field': {
+                'abstract': True,
+                'description': "basic fields"
+            },
+            'investigation field': {
+                'abstract': True,
+                'description': "field describing aspect of the investigation/study to which the sample belongs"
+            },
+            'nucleic acid sequence source field': {
+                'abstract': True
+            },
+            'sequencing field': {
+                'abstract': True
+            },
+            'mixs extension field': {
+                'abstract': True
+            },
+            'environment field': {
+                'abstract': True,
+                'description': "field describing environmental aspect of a sample"
+            }
+        }
+        classes = {}
+        subsets = {}
+        enums = {}
+        obj = {
+            'id': f'http://w3id.org/mixs',
+            'name': 'MIxS',
+            'description': 'Minimal Information about any Sequence Standard',
+            'imports': [
+                'linkml:types',
+                'checklists'
+            ],
+            'prefixes': {
+                'linkml': 'https://w3id.org/linkml/',
+                'mixs.vocab': 'https://w3id.org/mixs/vocab/',
+                'MIXS': 'https://w3id.org/mixs/terms/',
+            },
+            'default_prefix': 'mixs.vocab',
+            'slots': {},
+            'classes': classes,
+            'subsets': subsets
+        }
+
+
+        # TODO: make configurable whether this is in main schema or import
+        rschema = new_schema('ranges')
+        for k,v in datatype_schema.items():
+            rschema[k] = v
+        self.save_schema(rschema, 'ranges.yaml')
+        
+        cls_slot_req = {}
+        slot_cls_req = {}
+
+        core_slots = []
+        core_env_slots = []
+
+        core_slot_dict = {}
+        for _, row in core_df.iterrows():
+            s_id, slot = self.create_slot(row, enums=enums)
+            if s_id is None:
+                continue
+            slots[s_id] = slot
+            core_slot_dict[s_id] = row
+            core_slots.append(s_id)
+            if row['Section'] == 'environment':
+                core_env_slots.append(s_id)
+
+        for checklist, info in CHECKLISTS.items():
+            checklist_slot_usage = {}
+            checklist_name = info['name']
+            for s_id, s_row in core_slot_dict.items():
+                cardinality = s_row[checklist]
+                if cardinality != '-' and cardinality != 'E':
+                    usage = {}
+                    checklist_slot_usage[s_id] = usage
+                    if cardinality == 'M':
+                        usage['required'] = True
+                    elif cardinality == 'X':
+                        usage['required'] = False
+                    elif cardinality == 'C':
+                        usage['comments'] = ['conditional mandatory']
+            classes[checklist_name] = {
+                'mixin': True,
+                'description': info['fullname'],
+                'aliases': [
+                    info['abbrev']
+                ],
+                #'todos': ['add details here'],
+                'slots': list(checklist_slot_usage.keys()),
+                'slot_usage': checklist_slot_usage
+            }
+        classes[CORE_PACKAGE_NAME] = {
+            'description': 'Core package. Do not use this directly, this is used to build other packages',
+            'slots': core_slots
+        }
+        env_packages = []
+        for _, row in pkg_df.iterrows():
+            p = row['Environmental package']
+            req = row['Requirement']
+            is_required = req == 'M'
+            cn = safe(p.lower())
+            if cn not in classes:
+                env_packages.append(cn)
+                cls_slot_req[cn] = {}
+                classes[cn] = {
+                    #'is_a': CORE_PACKAGE_NAME,
+                    'description': p,
+                    'mappings': [],
+                    'slots': list(core_env_slots),
+                    'slot_usage': {}
+                }
+            c = classes[cn]
+
+            s_id, slot = self.create_slot(row, enums=enums)
+
+            if s_id is not None:
+                # TODO: this makes the documentation odd
+                #c['slot_usage'][s_id] = {'required': is_required}
+                cls_slot_req[cn][s_id] = req
+
+                if s_id not in slots:
+                    slots[s_id] = slot
+                else:
+                    None ## TODO: compare
+                if s_id not in slot_cls_req:
+                    slot_cls_req[s_id] = {}
+                slot_cls_req[s_id][cn] = req
+                if s_id not in core_slots:
+                    c['slots'].append(s_id)
+
+        n_cls = len(cls_slot_req.keys())
+        inf_core_slots = []
+        for s_id, s in slot_cls_req.items():
+            packages_str = ', '.join(list(s.keys()))
+            if len(s.keys()) == n_cls:
+                inf_core_slots.append(s_id)
+                cmt = "This field is used in all packages"
+            elif len(s.keys()) == 1:
+                cmt = f"This field is used uniquely in: {packages_str}"
+            else:
+                cmt = f"This field is used in: {len(s.keys())} packages: {packages_str}"
+            slots[s_id]['comments'].append(cmt)
+
+
+        for p in env_packages:
+            for checklist, info in CHECKLISTS.items():
+                name = info['name']
+                fullname = info['fullname']
+                combo = f'{p} {name}'
+                classes[combo] = {
+                    'is_a': p,
+                    'mixins': [name],
+                    'description': f'Combinatorial checklist {fullname} with environmental package {p}'
+                }
+            pname = underscore(p).replace("-", "_")
+            obj['imports'].append(pname)
+            pschema = new_schema(pname)
+            pschema['imports'].append('terms')
+            pschema['classes'] = {p: classes[p]}
+            del classes[p]
+            self.save_schema(pschema, f'{pname}.yaml')
+
+
+        slot_schema = new_schema('terms')
+        slot_schema['imports'].append('ranges')
+        slot_schema['slots'] = slots
+        slot_schema['enums'] = enums
+        self.save_schema(slot_schema, 'terms.yaml')
+
+        checklist_schema = new_schema('checklists')
+        checklist_schema['imports'].append('terms')
+        checklist_schema['classes'] = obj['classes']
+        obj['classes'] = {}
+        self.save_schema(checklist_schema, 'checklists.yaml')
+
+        return obj
+
+@click.command()
+def cli(**kwargs):
+    """ Generate JSON Schema representation of a biolink model """
+    cv = MIxS6Converter()
+    cv.core_filename = f'downloads/mixs6_core.tsv'
+    cv.packages_filename = f'downloads/mixs6.tsv'
+    cv.convert_and_save('src/schema/mixs.yaml')
+
+
+if __name__ == '__main__':
+    cli()
+    
