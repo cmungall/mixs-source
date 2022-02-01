6abd34be450f36d26f48e383a7d8acb6d3177221
Mark Andrew Miller <MAM@lbl.gov>
2022-02-01 18:59:59 -0500

    mkdocs.log

--- mixs.py
+++ mixs.py
@@ -1,5 +1,5 @@
 # Auto generated from mixs.yaml by pythongen.py version: 0.9.0
-# Generation date: 2022-02-01T15:24:55
+# Generation date: 2022-02-01T17:36:34
 # Schema: MIxS
 #
 # id: http://w3id.org/mixs
